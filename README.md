2e900daf677c3c2ce127998437706a9f74a57ba5
邹慧东 <huidong.zou@niu.com>
2016-10-08 23:45:44 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # gm-captcha
-A node captcha library using the gm module 
+
+a node captcha library using the gm module 
+
+gm is the GraphicsMagick and ImageMagick for node
+
+[gm document](https://github.com/aheckmann/gm)
+
+
+## Getting started
+
+First download and install [GraphicsMagick](http://www.graphicsmagick.org/) or [ImageMagick](http://www.imagemagick.org/). In Mac OS X, you can simply use [Homebrew](http://mxcl.github.io/homebrew/) and do:
+
+    brew install imagemagick
+    brew install graphicsmagick
+
+then either use npm:
+
+	npm install gm-captcha
+
+
+## Usage
+
+```js
+var gmCaptcha = require('gm-captcha');
+var options = {
+	width: 100,
+	height: 50,
+	text: '1024'
+}
+// Other configurations using default parameters
+var captcha  = new gmCaptcha();
+var gmObj = captcha.generator(); // return a gm object
+captcha.gmBuffer(gmObj, 'PNG', function (buffer) {
+	// do something with buffer
+});
+```
+## Options
+
+<table>
+	<th>filed</th><th>description</th><th>default</th>
+    <tr><td>width</td><td>image width</td><td>100</td></tr>
+    <tr><td>height</td><td>image height</td><td>50</td></tr>
+    <tr><td>background</td><td>image background, support rgb or hex</td><td>#fff</td></tr>
+    <tr><td>colorLength</td><td>the maximum color count of the point and line displayed</td><td>20</td></tr>
+    <tr><td>text</td><td>the text you want to display</td><td>Random four digit</td></tr>
+    <tr><td>textColor</td><td>text color, support rgb or hex</td><td>#000</td></tr>
+    <tr><td>textIndent</td><td>indent, left margin of text</td><td>0</td></tr>
+    <tr><td>wordSpacing</td><td>word space</td><td>25</td></tr>
+    <tr><td>maxSwirl</td><td>the maximum degree of the picture center swirl</td><td>20</td></tr>
+    <tr><td>maxShear</td><td>the maximum degree of the picture shear</td><td>30</td></tr>
+    <tr><td>lineCount</td><td>the maximum count of line</td><td>20</td></tr>
+    <tr><td>lineWidth</td><td>the width of line</td><td>1</td></tr>
+    <tr><td>pointCount</td><td>the maximum count of point</td><td>500</td></tr>
+    <tr><td>font</td><td>font path</td><td>DroidSansFallback</td></tr>
+    <tr><td>fontSize</td><td>font size</td><td>50</td></tr>
+</table>
+
+## License
+
+MIT License
+
+Copyright (c) [2016] [eidonjoe]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,95 @@
+var gm = require('gm');
+var generateCode = require('./lib/generateCode.js');
+var getColor = require('./lib/getColors.js');
+var randomBetween = require('./lib/randomBetween.js');
+var path = require('path');
+var rootPath = path.join(__dirname,'./');
+function Gm(options) {
+	// defaults
+	this.options = options || {};
+	this.options.colorLength = this.options.colorLength || 20
+	this.options.textLength = this.options.textLength || 4
+
+	if (!this.options.symbol) this.options.symbol = '';
+	if (!this.options.width) this.options.width = 100;
+	if (!this.options.height) this.options.height = 50;
+	if (!this.options.background) this.options.background = '#fff';
+	if (!this.options.colors) this.options.colors = getColor(this.options.colorLength);
+	if (!this.options.text) this.options.text = generateCode(this.options.textLength);
+	if (!this.options.textColor) this.options.textColor = '#000';
+	if (!this.options.textIndent) this.options.textIndent = 0;
+	if (!this.options.wordSpacing) this.options.wordSpacing = 25;
+	if (!this.options.maxSwirl && this.options.maxSwirl != 0) this.options.maxSwirl = 20;
+	if (!this.options.maxShear && this.options.maxShear != 0) this.options.maxShear = 30;
+	if (!this.options.lineCount) this.options.lineCount = 20;
+	if (!this.options.lineWidth) this.options.lineWidth = 1;
+	if (!this.options.pointCount) this.options.pointCount = 500;
+	if (!this.options.font) this.options.font = rootPath + 'font/DroidSansFallback.ttf'; // 不识别绝对路径
+	if (!this.options.fontSize) this.options.fontSize = 50;
+}
+Gm.prototype.init = function () {
+	this.gm = gm(this.options.width, this.options.height, this.options.background)
+			.in({width: this.options.width,height: this.options.height});
+
+}
+Gm.prototype.drawText = function () {
+	for (var i = 0; i < this.options.text.length; i++) {
+		this.options.symbol = randomBetween(0,10) > 5 ? '' : '-';
+		this.gm
+		.stroke(this.options.textColor, randomBetween(1,3))
+		.drawText(
+			parseInt(i * this.options.wordSpacing), 
+			this.options.fontSize, this.options.text[i]
+		)
+		.font(this.options.font)
+		.fontSize(this.options.fontSize)
+		.swirl(this.options.symbol + randomBetween(0,this.options.maxSwirl))
+		.shear(
+			this.options.symbol + randomBetween(0,this.options.maxShear),
+			this.options.symbol + randomBetween(0,this.options.maxShear)
+		)
+	}
+	return this
+}
+Gm.prototype.drawLine = function () {
+	for (var i = 0; i < this.options.lineCount; i++) {
+		this.gm
+		.stroke(this.options.colors[randomBetween(0,this.options.colors.length)].css, this.options.lineWidth)
+		.drawLine(
+			randomBetween(0, 2 * this.options.width),
+			randomBetween(0, 2 * this.options.width),
+			randomBetween(0, 2 * this.options.width),
+			randomBetween(0, 2 * this.options.width))
+	}
+	return this
+}
+Gm.prototype.drawPoint = function () {
+	for (var i = 0; i < this.options.pointCount; i++) {
+		this.gm
+		.stroke(this.options.colors[randomBetween(0,this.options.colors.length)].css, 1)
+		.drawPoint(
+			randomBetween(0, 2 * this.options.width),
+			randomBetween(0, 2 * this.options.width))
+	}
+	return this
+}
+Gm.prototype.gmBuffer = function (gm, bufferType, cb) {
+	gm.toBuffer(bufferType,function (err, buffer) {
+		return cb(buffer);
+	})
+}
+Gm.prototype.gmFile = function (gm, path, filename, fileType) {
+	gm.write(path + filename + '.' + fileType, function (err) {
+		console.log(err);
+		if (err) return err;
+	})
+}
+Gm.prototype.generator = function () {
+	this.init();
+	this.drawText();
+	this.drawLine();
+	this.drawPoint();
+	this.gm.resize(this.options.width, this.options.height, "!"); // necessary!
+	return this.gm;
+}
+module.exports = Gm;
