d3742e5e0e65080f89b9e8b86aa369131f27a286
eidonjoe <806488716@qq.com>
2016-10-09 09:46:12 +0800

    update test

--- test.js
+++ test.js
@@ -1,22 +1,17 @@
 'use strict'
 
-var http = require('http');
-var port = 9999;
 var gmCaptcha = require('./index.js');
 
-http.createServer(function(req,res){
-	var options = {
-		width: 100,
-		height: 50,
-		text: '1024'
-	}
-	// Other configurations using default parameters
-	var captcha  = new gmCaptcha(options);
-	var gmObj = captcha.generator(); // return a gm object
-	captcha.gmBuffer(gmObj, 'PNG', function (buffer) {
-		// do something with buffer
-		res.end(buffer);
-	});
-
-}).listen(port);
-console.log('listen in port' + port,'http://localhost:' + port);
+var options = {
+	width: 100,
+	height: 50,
+	text: '1024'
+}
+// Other configurations using default parameters
+var captcha  = new gmCaptcha(options);
+var gmObj = captcha.generator(); // return a gm object
+captcha.gmBuffer(gmObj, 'PNG', function (buffer) {
+	// do something with buffer
+	console.log(buffer);
+	return buffer
+});
